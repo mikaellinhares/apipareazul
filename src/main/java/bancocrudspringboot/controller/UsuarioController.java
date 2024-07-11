f414e47ecff052e5a33020d359b8dee423ae9d58
Mikael <mikael.linhares@gmail.com>
2024-07-11 20:58:55 -0300

    .

--- src/main/java/bancocrudspringboot/controller/UsuarioController.java
+++ src/main/java/bancocrudspringboot/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package bancocrudspringboot.controller;
 
+import bancocrudspringboot.model.Cartao;
 import bancocrudspringboot.model.Usuario;
 import bancocrudspringboot.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
@@ -26,5 +28,52 @@ public class UsuarioController {
 	@ResponseStatus(HttpStatus.OK)
 	public List<Usuario> getAllCadastros() {
 		return this.usuarioRepository.findAll();
+	}
+
+	//
+	public class AdicionarSaldoRequest {
+		private Long usuario;
+		private Long cartao;
+		private Double valor;
+	
+		// Getters e Setters
+		// Usuario
+		public Long getUsuario() {
+			return usuario;
+		}
+	
+		public void setUsuario(Long usuario) {
+			this.usuario = usuario;
+		}
+
+		// Cartao
+		public Long getCartao() {
+			return cartao;
+		}
+	
+		public void setCartao(Long cartao) {
+			this.cartao = cartao;
+		}
+		
+		// Valor
+		public Double getValor() {
+			return valor;
+		}
+	
+		public void setValor(Double valor) {
+			this.valor = valor;
+		}
+	}
+
+	@PutMapping("/usuarios/saldo/adicionar")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Usuario> adicionarSaldo(@RequestBody AdicionarSaldoRequest dados){
+		Usuario usuario = this.usuarioRepository.findById(dados.usuario);
+		// Optional<Cartao> cartao = this.cartaoRepository.findById(dados.cartao);
+		//
+		usuario.setSaldo(dados.valor);
+		ResponseEntity.ok(this.cartaoRepository.save(cadastro));
+		//
+		return this.usuarioRepository.save(cartao);
 	}
 }
